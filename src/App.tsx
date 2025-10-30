45a2b9860c051b14497db2fc36b9c1622ec60210
contactmailvisa-beep <contact.mail.visa@gmail.com>
2025-10-30 07:14:21 +0300

    Add App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Auth from "./pages/Auth";
+import Dashboard from "./pages/Dashboard";
+import Project from "./pages/Project";
+import ProjectPreview from "./pages/ProjectPreview";
+import ProfileView from "./pages/ProfileView";
+import ProfileAnalytics from "./pages/ProfileAnalytics";
+import Checkout from "./pages/Checkout";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/auth" replace />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/project/:id" element={<Project />} />
+          <Route path="/profile-analytics/:username" element={<ProfileAnalytics />} />
+          <Route path="/:userId/:projectSlug" element={<ProjectPreview />} />
+          <Route path="/:username" element={<ProfileView />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
